c4792295da1f327bf71b943e9b6e2d9708622a60
mengshanli <50234977+mengshanli@users.noreply.github.com>
2025-04-04 16:54:19 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    const image_folder='./images/';
+    const category_en=['sashimi','donburi','sushi','handroll','others'];
+    // const category_cn=['刺身','冷食丼飯','握壽司','手卷','單點'];
+    const each_count=[2,2,2,2,2]; //每一個種類有幾張圖片，如有增減圖片，需人工手動修改
+
+    for (let i=0; i<category_en.length; i++){
+      for (let j=1; j<each_count[i]+1; j++){
+        let image_src=image_folder+category_en[i]+'/'+j+'.jpeg';
+        let category=document.getElementById(category_en[i]);
+
+        const food = document.createElement("div");
+        food.innerHTML = `        
+          <img class='col-md-5 col-10 my-3' src="${image_src}" alt="${category_en[i]+j}">
+        `;
+        category.parentNode.appendChild(food);
+      }
+    }
+
+});
+
